53f14fe5e78b6617a57d9c83e7f4860a4816788e
Schlabonski <fthielemann@gmx.de>
2017-02-13 10:50:01 +0100

    create package structure

---
+++ customstuff/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(name='Customstuff',
+        version='0.1',
+        description='Frequently used fitting functions etc.',
+        author='Fabian Thielemann',
+        packages=['customstuff']
+      )
